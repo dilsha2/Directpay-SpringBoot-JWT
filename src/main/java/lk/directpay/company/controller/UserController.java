91cf57e3bf284ba66af53fcad14394026d2c4679
unknown <diliprasanna96@gmail.com>
2023-07-04 14:26:29 +0530

    update user detail end point

--- src/main/java/lk/directpay/company/controller/UserController.java
+++ src/main/java/lk/directpay/company/controller/UserController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -48,10 +49,8 @@ public class UserController {
     }
 
     @GetMapping("/user-details")
-    public ResponseEntity<?> getUserDetails() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+    public ResponseEntity<?> getUserDetails(@AuthenticationPrincipal UserDetails userDetails) {
+        if (userDetails != null) {
             return ResponseEntity.ok(userDetails);
         } else {
             String errorMessage = "User details not found";
